e044f00bfa7eb7feff1528317a4fa53e64146362
Cédric Caspar <cedric.caspar@gmail.com>
2017-09-13 23:54:35 +0200

    Implémentation de PATCH /:collection/:id

--- app/routes/collection.js
+++ app/routes/collection.js
@@ -15,13 +15,22 @@ router.get('/:collection/:id',function(req,res){
 });
 
 router.post('/:collection',function(req,res){
-    //créé un nouvel élément
+    //créé un nouvel élément    
     res.send('créé un '+req.params.collection);
 });
 
 router.patch('/:collection/:id',function(req,res){
-    //met à jour un élément
-    res.send('met à jour '+req.params.collection+" id "+req.params.id);
+    //met à jour un élément    
+    let query = req.body;    
+    query['criteres']={};    
+    query['criteres']['id_'+req.params.collection] = req.params.id;    
+
+    req.spip.update(req.params.collection,query)
+    .then((retour)=>{
+        console.log('retour',retour);
+        res.send(retour);
+    })
+    .catch((e)=>console.log("Erreur ",e));
 });
 
 router.delete('/:collection/:id',function(req,res){
