a014fba982adf0a08f2e30ae690aee7a93056560
Cédric Caspar <cedric.caspar@gmail.com>
2017-09-21 15:56:05 +0200

    Réglage des droits sur routes de publication

--- app/routes/publication.js
+++ app/routes/publication.js
@@ -1,25 +1,26 @@
-var express = require('express'), 
+const express = require('express'), 
+roles = require('../auth/roles'),
 router = express.Router();
 
 
 
-router.patch('/publie/:collection/:id',function(req,res){
+router.patch('/publie/:collection/:id',autorise(roles.ADMIN),function(req,res){
     //Met le statut d'un élément SPIP à "Publié en ligne"
 });
 
-router.patch('/refuse/:collection/:id',function(req,res){
+router.patch('/refuse/:collection/:id',autorise(roles.ADMIN),function(req,res){
     //Met le statut d'un élément SPIP à "Refusé"
 });
 
-router.patch('/poubelle/:collection/:id',function(req,res){
+router.patch('/poubelle/:collection/:id',autorise(roles.ADMIN),function(req,res){
     //Met le statut d'un élément SPIP à "à la poubelle"
 });
 
-router.patch('/redac/:collection/:id',function(req,res){
+router.patch('/redac/:collection/:id',autorise(roles.REDACTEUR),function(req,res){
     //Met le statut d'un élément SPIP à "en cours de rédaction"
 });
 
-router.patch('/eval/:collection/:id',function(req,res){
+router.patch('/eval/:collection/:id',autorise(roles.REDACTEUR),function(req,res){
     //Met le statut d'un élément SPIP à "proposé à l'évaluation"
 });
 
