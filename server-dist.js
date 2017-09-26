da70320279a0539d3b12181630edd330be494edc
Cédric Caspar <cedric.caspar@gmail.com>
2017-09-26 16:52:51 +0200

    On ajoute un exemple de route custom et on appel la methode endConfig

--- server-dist.js
+++ server-dist.js
@@ -9,6 +9,13 @@ app.set('x-powered-by', serverParam.xPoweredBy);
 
 const spip = new SpipServer(app,serverParam.racine);
 
+//on ajoute une route custom qui utilisera le système d'authentifcation et de droits de spip
+spip.router.get('/coffee',autorise(roles.PUBLIC),function(req,res){
+    res.status(418).send("I'm a teapot");
+});
+//on a fini d'ajouter nos routes custom, donc on finalise la config des routes spip (aka, ts autre appel de route passe en 404)
+spip.endConfig();
+
 app.listen(serverParam.port,()=>{   
     console.log('Serveur écoute sur le port '+serverParam.port);    
 });
